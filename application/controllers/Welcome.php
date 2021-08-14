ae22304d0df30f45c0a0c92586c8277b0fc10086
Ariel <abeltran.9476@gmail.com>
2021-08-14 19:12:27 -0400

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -109,7 +109,7 @@ class Welcome extends CI_Controller
 			if (substr($text, 0, 9) == '/register') {
 				$groupIn =  trim(preg_replace('/\/register/', '', $text));
 
-				$keyboard = [[' Registrar ' => '/register']];
+				$keyboard = [' Registrar ' => '/register'];
 
 				$reply_markup = $telegram->replyKeyboardMarkup([
 					'keyboard' => $keyboard,
