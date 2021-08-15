3b7e26f3c016351c1ad412aaa1b44d60af32158a
Ariel <abeltran.9476@gmail.com>
2021-08-15 12:00:58 -0400

    Update Groups_model.php

--- application/models/Groups_model.php
+++ application/models/Groups_model.php
@@ -31,6 +31,9 @@ class Groups_model extends CI_Model
 		$this->db->select('user');
 		$this->db->where('group_name', $group);
 		$query = $this->db->get('groups');
-		return $query->rows();
+		if ($query->num_rows() > 0) {
+			return true;
+		}
+		return false;
 	}
 }
