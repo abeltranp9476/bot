77cdd6f1f7ee36b0482ccd48475ad2bd504c7c01
Ariel <abeltran.9476@gmail.com>
2021-08-16 18:00:55 -0400

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -508,8 +508,16 @@ class Welcome extends CI_Controller
 
 				$telegram->sendMessage([
 					'chat_id' => $chatId,
-					'text' => "Escriba su mensaje personalizado:",
-					'reply_markup' => $reply_markup
+					'text' => "Escriba su mensaje personalizado:<br>
+					<br>
+					<b>Comodines:</b><br>
+					%user% - Nombre de usuario<br>
+					%total% - # de contactos a añadir<br>
+					%counter% - # de contactos añadidos<br>
+					%reminder% - # de contactos que faltan
+					",
+					'reply_markup' => $reply_markup,
+					'parse_mode' => 'html'
 				]);
 
 				exit;
